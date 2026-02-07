4384c7e1b9b3aaa95ea52f6e2bf025d80ebd1028
Arman Khan <sparmankhan04@gmail.com>
2026-02-07 18:38:48 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/[username]/admin/layout.tsx
+++ src/app/[username]/admin/layout.tsx
@@ -4,14 +4,14 @@ import AdminSidebar from '@/components/admin-sidebar';
 
 export default function AdminLayout({
   children,
-  params,
+  params: { username },
 }: {
   children: React.ReactNode;
   params: { username: string };
 }) {
   return (
     <div className="fixed inset-0 bg-background z-50 grid w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <AdminSidebar username={params.username} />
+      <AdminSidebar username={username} />
       <div className="flex flex-col">
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 overflow-auto">
           {children}
