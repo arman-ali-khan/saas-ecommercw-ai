7aa2fede9cd71d50bf5854b7c5596f7158d69090
Arman Khan <sparmankhan@gmail.com>
2026-02-14 08:13:54 +0000

    remove padding

--- src/components/site-layout.tsx
+++ src/components/site-layout.tsx
@@ -36,7 +36,7 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
       <div className="flex flex-col min-h-screen">
         <CustomerAuthInitializer />
         <Header />
-        <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-24 md:pb-8">
+        <main className="flex-grow container mx-auto px-1 sm:px-6 lg:px-8 py-8 pb-24 md:pb-8">
           {children}
         </main>
         <Footer />
