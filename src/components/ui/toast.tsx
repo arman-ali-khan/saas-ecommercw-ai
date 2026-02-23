7be00c9b15b46d70caf96e4c0073b651242b969d
Arman Khan <sparmankhan@gmail.com>
2026-02-23 10:32:35 +0000

    update toast. toast show bottom left on desktop and in mobile view show

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed h-auto max-h-fit z-[100] flex max-h-fit w-full flex-col-reverse p-1 sm:p-4 sm:flex-col md:max-w-[420px]",
+      "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4 bottom-20 sm:bottom-4 sm:left-4 sm:top-auto sm:right-auto sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
