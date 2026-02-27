4a19c916e6b10e56f8a0d4b1f1af9b330061862e
Arman Khan <sparmankhan@gmail.com>
2026-02-27 22:25:16 +0000

    https://woocommercecore.mystagingwebsite.com

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'woocommercecore.mystagingwebsite.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
